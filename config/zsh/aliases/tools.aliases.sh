530e9435ecdd1bf173eafdb397a9e474803f8438
Dmitry Fisenko <fisenkodv@gmail.com>
2026-07-16 00:11:17 -0400

    feat: scripts reorg

--- config/zsh/aliases/tools.aliases.sh
+++ config/zsh/aliases/tools.aliases.sh
@@ -5,11 +5,11 @@ if type bat >/dev/null 2>&1; then
 fi
 
 if type eza >/dev/null 2>&1; then
-  alias ll 'eza --long --header --git --group --icons --time-style long-iso'
-  alias la 'll --all'
-  alias tr1 'eza --tree --level=1 --icons'
-  alias tr2 'eza --tree --level=2 --icons'
-  alias tr3 'eza --tree --level=3 --icons'
+  alias ll='eza --long --header --git --group --icons --time-style long-iso'
+  alias la='ll --all'
+  alias tr1='eza --tree --level=1 --icons'
+  alias tr2='eza --tree --level=2 --icons'
+  alias tr3='eza --tree --level=3 --icons'
 else
   alias ll='ls -la'
 fi
